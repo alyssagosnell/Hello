6fbad418f5a63e2563bedc1736686717ba074c35
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-07 14:39:33 -0400

    Lab 2

---
+++ sdi-lab2/Gosnell_Alyssa_Lab2.js
@@ -0,0 +1,14 @@
+alert("JavaScript works!");
+
+// Variables
+var partOne = "I woke up late and got ready for class";
+var partTwo = "I fed the animals \"Sacha and Oliver\" before leaving";
+
+var question = confirm("Did I make it to class on time? Click OK for true, Cancel for false.");
+var time = prompt("How long did it take to get to class?");
+
+//Outputs
+console.log("This morning " + partOne + ".");
+console.log("After that " + partTwo + ".");
+console.log("It is " + question + " that I made it to class on time.");
+console.log("You said " + time + " minutes but it was actually 2 minutes.");
