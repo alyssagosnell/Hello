fda64a4f1d81fe40ca1630c3f21f60af605f0b06
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-16 14:26:19 -0400

    LAB 6 PART 2

--- Lab6/Gosnell_Alyssa_Lab6.js
+++ Lab6/Gosnell_Alyssa_Lab6.js
@@ -13,7 +13,21 @@ function vgArray() {
     //Local Variables
     var videoGame = ["CoD: Black Ops", "The Evil Within", "Jak and Daxter", "Fallout", "American McGee's Alice"];
     var videoGameGenres = ["action", "survival", "adventure", "RPG", "horror"];
+    var poppedGame;
+    var poppedGenre;
 
+
+    //Loop
+
+    for (var i =0; i < videoGame.length; i++) {
+
+        console.log( videoGame[i] + " is a(n) " + videoGameGenres[i] + " type game.");
+    }
+
+    poppedGame = videoGame.pop();
+    poppedGenre = videoGameGenres.pop();
+
+    
 
 //MAIN CODE
 
