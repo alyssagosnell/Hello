b6834450d725f521eaf91ef070b5979f02026d61
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-16 14:23:59 -0400

    LAB 6 PART 1

---
+++ Lab6/Gosnell_Alyssa_Lab6.js
@@ -0,0 +1,20 @@
+//Alyssa Gosnell
+//SDI 1505
+//LAB SIX
+//05/16/15
+
+//alert("JavaScript works!");
+
+
+
+//FUNCTION
+function vgArray() {
+
+    //Local Variables
+    var videoGame = ["CoD: Black Ops", "The Evil Within", "Jak and Daxter", "Fallout", "American McGee's Alice"];
+    var videoGameGenres = ["action", "survival", "adventure", "RPG", "horror"];
+
+
+//MAIN CODE
+
+vgArray();
