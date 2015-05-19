e968809fa840946ab6305f63b5bd3f34e56f6256
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-19 10:18:27 -0400

    Lecture Example Commit 1

---
+++ Day7/js/Script.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Alyssa on 5/19/15.
+ */
+
+//alert("JavaScript works!");
+
+//Global variables
+var returnedArray = [];
+var numberArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
+
+
+//Function
+function evenArray(argArray){
+
+    //Local Variables
+    var newArray = [];
+    var modNum;
+
+    console.log(argArray);
+
+
+}
+
+//Main Code
+returnedArray = evenArray(numberArray);
