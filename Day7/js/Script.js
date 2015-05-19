ab3db56123d99c57f5d3e6aee23577f5272bfc0f
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-19 10:48:28 -0400

    Lecture Example Commit 3 (Even and Odd)

--- Day7/js/Script.js
+++ Day7/js/Script.js
@@ -7,20 +7,32 @@
 //Global variables
 var returnedArray = [];
 var numberArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
+var userPrompt = prompt("Enter 'odd' to return odd numbers or 'even' to return even numbers.");
+
 
 
 //Function
-function evenArray(argArray){
+function evenArray(argArray, oddOrEven) {
 
     //Local Variables
     var newArray = [];
     var modNum;
+    var zeroOrOne;
 
-    for (i = 0; i < argArray.length; i++){
+    if (oddOrEven === "odd") {
+
+        zeroOrOne = 1;
+    } else {
+
+        zeroOrOne = 0;
+
+    }
+
+    for (i = 0; i < argArray.length; i++) {
 
         modNum = argArray [i] % 2;
 
-        if (modNum === 0) {
+        if (modNum === zeroOrOne) {
 
             newArray.push(argArray[i]);
 
@@ -33,6 +45,6 @@ function evenArray(argArray){
 }
 
 //Main Code
-returnedArray = evenArray(numberArray);
+returnedArray = evenArray(numberArray, userPrompt);
 
-console.log("The even number array is " + returnedArray);
+console.log("The " + userPrompt + " number array is " + returnedArray);
