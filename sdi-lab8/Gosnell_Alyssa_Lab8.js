b4c820838b71798c1ac71d20bf578a2c58a2f4d3
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-21 17:23:29 -0400

    Lab 8 Problem 2 Commit 1

--- sdi-lab8/Gosnell_Alyssa_Lab8.js
+++ sdi-lab8/Gosnell_Alyssa_Lab8.js
@@ -4,44 +4,65 @@ Lab 8
 SDI 1505
 May 21, 2015
  */
+//
+////alert("JavaScript works!");
+//
+////PROBLEM ONE
+////Global Variables
+//var emailAddress;
+//
+////Function
+//function findSymbol(emailArg) {
+//
+//    var atSymbol;
+//    var findAt = emailArg.indexOf("@");
+//    var findAt2 = emailArg.lastIndexOf("@");
+//
+//    if (findAt >= 0 && findAt2 === findAt) {
+//
+//        atSymbol = true
+//
+//    } else {
+//
+//        atSymbol = false
+//    }
+//
+//    if (atSymbol === false || emailArg.indexOf(".") <= 0 || emailArg.indexOf(" ") >= 0)
+//    {
+//        console.log("The email you entered is not valid.");
+//
+//    } else {
+//
+//        console.log("The email you entered is valid.");
+//
+//    }
+//
+//}
+//
+//// Main Code
+//emailAddress = prompt("Enter an email address below: ");
+//
+//findSymbol(emailAddress);
 
-//alert("JavaScript works!");
 
-//PROBLEM ONE
 
+//PROBLEM TWO
 //Global Variables
-var emailAddress;
+
+var firstString = "Blue, Yellow, Green, Orange";
+var comma = ",";
+var slash = "/";
+
 
 //Function
-function findSymbol(emailArg) {
 
-    var atSymbol;
-    var findAt = emailArg.indexOf("@");
-    var findAt2 = emailArg.lastIndexOf("@");
+function replacedSeparator(string, sep1, sep2) {
 
-    if (findAt >= 0 && findAt2 === findAt) {
+    
 
-        atSymbol = true
-
-    } else {
-
-        atSymbol = false
-    }
-
-    if (atSymbol === false || emailArg.indexOf(".") <= 0 || emailArg.indexOf(" ") >= 0)
-    {
-        console.log("The email you entered is not valid.");
-
-    } else {
-
-        console.log("The email you entered is valid.");
-
-    }
-
-
+    return string;
 }
 
-// Main Code
-emailAddress = prompt("Enter an email address below: ");
 
-findSymbol(emailAddress);
+//Main Code
+console.log(replacedSeparator(firstString, comma, slash));
