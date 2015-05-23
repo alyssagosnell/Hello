32428f9fb783c79c8737073045a6504021cc1a2a
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-23 14:21:54 -0400

    Lab 9 Commit 1

---
+++ sdi-lab9/Gosnell_Alyssa_Lab9.js
@@ -0,0 +1,21 @@
+//alert("JavaScript works!");
+
+//PROBLEM ONE - FORMAT A NUMBER
+
+//Global Variables
+var firstNumber;
+var decimalPoint;
+
+
+
+
+//FUNCTION
+function moveDecimal(numberArg, decimalArg) {
+
+
+}
+
+//MAIN CODE
+firstNumber = parseFloat(prompt("Please enter the number you wish to format: ","123.56"));
+decimalPoint = parseInt(prompt("Please enter a number you wish to move the decimal point: ","5"));
+
