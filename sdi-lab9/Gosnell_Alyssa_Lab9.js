3ff0752c32393ce39e18b30d7a353835a95a3514
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-23 14:25:21 -0400

    Lab 9 Commit 2
    Finished Problem 1

--- sdi-lab9/Gosnell_Alyssa_Lab9.js
+++ sdi-lab9/Gosnell_Alyssa_Lab9.js
@@ -5,17 +5,23 @@
 //Global Variables
 var firstNumber;
 var decimalPoint;
-
+var returnNumber;
 
 
 
 //FUNCTION
 function moveDecimal(numberArg, decimalArg) {
 
+    var num1 = numberArg.toFixed(decimalArg);
 
+    return num1;
 }
 
 //MAIN CODE
 firstNumber = parseFloat(prompt("Please enter the number you wish to format: ","123.56"));
+
 decimalPoint = parseInt(prompt("Please enter a number you wish to move the decimal point: ","5"));
 
+returnNumber = moveDecimal(firstNumber, decimalPoint);
+
+console.log("This is the formatted number: " + returnNumber + ".");
