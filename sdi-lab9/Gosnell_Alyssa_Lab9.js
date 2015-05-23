f9ae6092d0251b6a6597e3964d760e074250988b
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-23 15:07:01 -0400

    Lab 9 Commit 3

--- sdi-lab9/Gosnell_Alyssa_Lab9.js
+++ sdi-lab9/Gosnell_Alyssa_Lab9.js
@@ -1,27 +1,51 @@
 //alert("JavaScript works!");
 
-//PROBLEM ONE - FORMAT A NUMBER
+////PROBLEM ONE - FORMAT A NUMBER
+//
+////Global Variables
+//var firstNumber;
+//var decimalPoint;
+//var returnNumber;
+//
+//
+//
+////FUNCTION
+//function moveDecimal(numberArg, decimalArg) {
+//
+//    var num1 = numberArg.toFixed(decimalArg);
+//
+//    return num1;
+//}
+//
+////MAIN CODE
+//firstNumber = parseFloat(prompt("Please enter the number you wish to format: ","123.56"));
+//
+//decimalPoint = parseInt(prompt("Please enter a number you wish to move the decimal point: ","5"));
+//
+//returnNumber = moveDecimal(firstNumber, decimalPoint);
+//
+//console.log("This is the formatted number: " + returnNumber + ".");
 
-//Global Variables
-var firstNumber;
-var decimalPoint;
-var returnNumber;
 
 
+//PROBLEM TWO - NUMBERS VS STRINGS
+
+//GLOBAL VARIABLES
+var string;
+var returnedNumber;
 
 //FUNCTION
-function moveDecimal(numberArg, decimalArg) {
+function numberFunction(numArg) {
 
-    var num1 = numberArg.toFixed(decimalArg);
+    while ()
 
-    return num1;
+
 }
 
+
 //MAIN CODE
-firstNumber = parseFloat(prompt("Please enter the number you wish to format: ","123.56"));
 
-decimalPoint = parseInt(prompt("Please enter a number you wish to move the decimal point: ","5"));
+string = prompt("Please enter a number: ","12");
 
-returnNumber = moveDecimal(firstNumber, decimalPoint);
+returnedNumber = numberFunction(string);
 
-console.log("This is the formatted number: " + returnNumber + ".");
