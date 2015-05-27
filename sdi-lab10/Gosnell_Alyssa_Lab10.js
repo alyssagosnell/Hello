626592ab68f0665e23cceda235feae6512d07450
Alyssa Gosnell <akgosnell@fullsail.edu>
2015-05-26 21:20:35 -0400

    Lab 10 Commit 1

---
+++ sdi-lab10/Gosnell_Alyssa_Lab10.js
@@ -0,0 +1,18 @@
+//Alyssa Gosnell
+//SDI 1505 Lab 10
+//May 26, 2015
+
+//alert("JavaScript works!");
+
+//Global Variables
+var userAnswer;
+var returnedVariable;
+
+
+//Function
+function lotteryFunction(userInput) {
+
+}
+
+//Main Code
+returnedVariable = lotteryFunction(userAnswer);
